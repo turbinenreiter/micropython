9f54d2c85261cda998e29aff4646c750ad7cb367
Sebastian Plamauer <oeplse@gmail.com>
2016-10-14 18:51:15 +0200

    changed storage info

--- stmhal/storage.c
+++ stmhal/storage.c
@@ -53,7 +53,7 @@
 
 #elif defined(STM32F401xE) || defined(STM32F411xE)
 
-STATIC byte flash_cache_mem[0x4000] __attribute__((aligned(4))); // 16k
+#define CACHE_MEM_START_ADDR (0x10000000) // CCM data RAM, 64k
 #define CACHE_MEM_START_ADDR (&flash_cache_mem[0])
 #define FLASH_SECTOR_SIZE_MAX (0x4000) // 16k max due to size of cache buffer
 #define FLASH_MEM_SEG1_START_ADDR (0x08004000) // sector 1
@@ -61,9 +61,8 @@ STATIC byte flash_cache_mem[0x4000] __attribute__((aligned(4))); // 16k
 
 #elif defined(STM32F412Zx)
 
-STATIC byte flash_cache_mem[0x4000] __attribute__((aligned(4))); // 16k
-#define CACHE_MEM_START_ADDR (&flash_cache_mem[0])
-#define FLASH_SECTOR_SIZE_MAX (0x4000) // 16k max due to size of cache buffer
+#define CACHE_MEM_START_ADDR (0x10000000)
+#define FLASH_SECTOR_SIZE_MAX (0x10000) // 64k max, size of CCM
 #define FLASH_MEM_SEG1_START_ADDR (0x08004000) // sector 1
 #define FLASH_MEM_SEG1_NUM_BLOCKS (224) // sectors 1,2,3,4: 16k+16k+16k+64k(of 64k)=112k
 
